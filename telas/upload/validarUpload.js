440f5e80b62a5dc0a38cd107215a120888809742
arthursouzaa <arthurjf.estudo@gmail.com>
2025-02-13 11:40:57 -0300

    continuando tela de upload *erro de validação

---
+++ telas/upload/validarUpload.js
@@ -0,0 +1,26 @@
+import { validarEntradas, contornarBordaInput } from '../conta/validacao.js';
+
+let inputs = document.querySelectorAll(".inpFormulario");
+const form = document.querySelector("form");
+
+for(const input of inputs) {
+    input.addEventListener("blur", function () {
+        contornarBordaInput(input);
+    })
+}
+
+form.addEventListener("submit", function (evento) {
+    for(const input of inputs) {
+        if (validarEntradas(input)) {
+            evento.preventDefault();
+            mostrarMensagem();
+        }
+    }
+});
+
+function mostrarMensagem() {
+    const span = document.querySelector("#resultado");
+    span.textContent = "Usuário ou senha incorretos.";
+    span.style.padding = "0.5rem";
+    span.style.color = "red";
+}
